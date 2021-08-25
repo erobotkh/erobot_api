14e240feaaa969732e6a31444f26fa6f155e01ff
theacheng <theacheng.g6@gmail.com>
2021-08-26 00:56:34 +0700

    close #5 add example data with seeder

---
+++ examples/posts_example.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    type: 'post',
+    title: '1How to change name & password of Arduino HC-05',
+    body: '1I Love robot 📋✏️️ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet diam gravida sed sagittis eu. Rhoncus ultricies gravida amet, fringilla scelerisque vitae. Rhoncus ultricies gravida amet, fringilla scelerisque vitae. This can be rich text display. Keep it normal text for now.',
+    author: {
+      type: 'user',
+      first_name: 'Sok',
+      last_name: 'San',
+    }
+  },
+  {
+    type: 'post',
+    title: '2How to change name & password of Arduino HC-05',
+    body: '2I Love robot 📋✏️️ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet diam gravida sed sagittis eu. Rhoncus ultricies gravida amet, fringilla scelerisque vitae. Rhoncus ultricies gravida amet, fringilla scelerisque vitae. This can be rich text display. Keep it normal text for now.',
+    author: {
+      type: 'user',
+      first_name: 'Ken',
+      last_name: 'Pao',
+    }
+  },
+  {
+    type: 'post',
+    title: '2How to change name & password of Arduino HC-05',
+    body: '2I Love robot 📋✏️️ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet diam gravida sed sagittis eu. Rhoncus ultricies gravida amet, fringilla scelerisque vitae. Rhoncus ultricies gravida amet, fringilla scelerisque vitae. This can be rich text display. Keep it normal text for now.',
+    author: {
+      type: 'user',
+      first_name: 'Leave',
+      last_name: 'ChinC',
+    }
+  },
+]
+
+export default posts
