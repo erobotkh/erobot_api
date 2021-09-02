fccfdb2f4198472eac3de2a8d498247505ebbbc1
theacheng <theacheng.g6@gmail.com>
2021-09-02 20:22:13 +0700

    close #13 refactor auth api

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/auth', authRoutes)
-app.use('/posts', verifyToken, postRoutes)
+app.use('/posts', postRoutes)
 
 const PORT = process.env.PORT || 3000
 
