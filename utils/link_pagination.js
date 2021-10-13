b9f3e4b4fda80cc50ea70b1c7557b7a236301370
Thea Choem <theacheng.g6@gmail.com>
2021-10-13 20:49:05 +0700

    close #16 link pagination (#19)

---
+++ utils/link_pagination.js
@@ -0,0 +1,30 @@
+const getUrl = (req) => {
+  const protocol = req.protocol;
+  const host = req.hostname;
+  const path = req.originalUrl.match('^[^?]*')[0].split('/').slice(1).join('/');
+  const port = process.env.PORT || PORT;
+
+  const fullUrl = `${protocol}://${host}:${port}/${path}`
+  return fullUrl;
+}
+
+const _getUrlFromPage = (page, req) => {
+  const _url = getUrl(req);
+  const query = req.query;
+
+  query['page'] = page;
+
+  const qs = new URLSearchParams(req.query);
+  return page != null ? _url + '?' + qs : null
+}
+
+const getTopLevelLinks = (docs, req) => {
+  return {
+    self: _getUrlFromPage(docs.page, req),
+    next: _getUrlFromPage(docs.nextPage, req),
+    prev: _getUrlFromPage(docs.prevPage, req),
+    last: _getUrlFromPage(docs.totalDocs, req),
+  }
+}
+
+export { getUrl, getTopLevelLinks }
