81931684b69ab40d4c98cb9d07a99612268ba9c0
Thea Choem <theacheng.g6@gmail.com>
2022-01-26 00:17:09 +0700

    close #31 add category api

---
+++ controllers/category_controller.js
@@ -0,0 +1,91 @@
+import asyncHandler from 'express-async-handler'
+import Category from '../models/category_model.js'
+import { buildItemsSerializer } from '../utils/json_serializer.js'
+
+const fetchCategories = (req, res) => asyncHandler(async (req, res) => {
+  const per_page = parseInt(req.query.per_page) || 20
+  const page = parseInt(req.query.page) || 1
+
+  const options = {
+    page: page,
+    limit: per_page,
+  }
+
+  const categories = await Category.paginate({}, options)
+  const _categories = buildItemsSerializer({
+    items: categories,
+    attributeSchema: Category.schema,
+    request: req,
+    relationships: []
+  })
+
+  res.send(_categories)
+})
+
+const createCategory = () => asyncHandler(async (req, res) => {
+  const category_name = req.body.name
+
+  if (!category_name) {
+    res.send({
+      message: 'Category name must not empty',
+    })
+    return;
+  }
+
+  const response = await Category.create({ name: category_name })
+  if (response) {
+    res.send({
+      message: 'Category created',
+      response: response,
+    })
+  } else {
+    res.send({
+      message: 'Can\'t create category',
+    })
+  }
+})
+
+const updateCategory = () => asyncHandler(async (req, res) => {
+  const category_name = req.body.name
+  const category_id = req.params.category_id
+
+  if (!category_name) {
+    res.send({
+      message: 'Category name must not empty',
+    })
+    return;
+  }
+
+  try {
+    const old_category = await Category.findById(category_id)
+    const response = await old_category.update({ name: category_name })
+    res.send({
+      message: 'Category updated',
+      response: response
+    })
+  } catch (error) {
+    res.send({
+      message: 'Update category fail',
+    })
+  }
+})
+
+// TODO: also delete its references to other posts
+const deleteCategory = () => asyncHandler(async (req, res) => {
+  const category_id = req.params.category_id
+  try {
+    const old_category = await Category.findById(category_id)
+    const response = await old_category.delete()
+    res.send({
+      message: 'Delete deleted',
+      response: response
+    })
+  } catch (error) {
+    res.send({
+      message: 'Delete category fail',
+      response: error,
+    })
+  }
+})
+
+export { fetchCategories, createCategory, updateCategory, deleteCategory }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import connectDB from './configs/db_configs.js'
 import postRoutes from './routes/post_routes.js'
 import commentRoutes from './routes/comment_routes.js'
 import memberRoutes from './routes/member_routes.js'
+import categoryRoute from './routes/category_route.js'
 import authRoutes from './routes/auth_routes.js'
 import renderEmailTemplate from './controllers/email_template_controller.js'
 
@@ -17,6 +18,7 @@ app.use('/auth', authRoutes)
 app.use('/posts', postRoutes)
 app.use('/comments', commentRoutes)
 app.use('/members', memberRoutes)
+app.use('/categories', categoryRoute)
 
 app.get('/email_template', renderEmailTemplate());
 
